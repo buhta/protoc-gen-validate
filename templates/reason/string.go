71ab364cf0dfcf248d882ad3f1fb577a5d8c8171
dauletisatayev <dake.99.10@gmail.com>
2021-03-10 16:03:44 +0600

    [reason] string: min & max length validators

--- templates/reason/string.go
+++ templates/reason/string.go
@@ -2,4 +2,11 @@ package reason
 
 const stringConstTpl = `{{ $f := .Field }}{{ $r := .Rules -}}`
 
-const stringTpl = `{{ $f := .Field }}{{ $r := .Rules -}}`
+const stringTpl = `{{ $f := .Field }}{{ $r := .Rules -}}
+{{- if $r.MinLen }}
+		let errors = errors @ (String.length(value) >= {{ $r.GetMinLen }} ? [] : ["length must be min " ++ string_of_int({{ $r.GetMinLen }})]);
+{{- end -}}
+{{- if $r.MaxLen }}
+		let errors = errors @ (String.length(value) <= {{ $r.GetMaxLen }} ? [] : ["length must be max " ++ string_of_int({{ $r.GetMaxLen }})]);
+{{- end -}}
+`
